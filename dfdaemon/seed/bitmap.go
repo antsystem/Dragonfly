c99fd590cc238fc124d398aa6ae3986d9bdff3cd
allen.wq <allen.wq@alipay.com>
2020-04-03 18:30:31 +0800

    add lock bitmap to support sync read/write

--- dfdaemon/seed/bitmap.go
+++ dfdaemon/seed/bitmap.go
@@ -3,6 +3,7 @@ package seed
 import (
 	"math"
 	"sync"
+
 	lbm "github.com/openacid/low/bitmap"
 )
 
@@ -10,12 +11,14 @@ type bitmap struct {
 	sync.RWMutex
 	bm          []uint64
 	maxBitIndex int32
+
+	lockMap map[int32]*lockInfo
 }
 
 func newBitMap(sizeOf64Bits int32, allSetBit bool) *bitmap {
 	bm := make([]uint64, sizeOf64Bits)
 	if allSetBit {
-		for i := 0; i < int(sizeOf64Bits); i ++ {
+		for i := 0; i < int(sizeOf64Bits); i++ {
 			bm[i] = math.MaxUint64
 		}
 	}
@@ -23,12 +26,121 @@ func newBitMap(sizeOf64Bits int32, allSetBit bool) *bitmap {
 	return &bitmap{
 		bm:          bm,
 		maxBitIndex: sizeOf64Bits * 64,
+		lockMap:     make(map[int32]*lockInfo),
 	}
 }
 
 // get gets the bits in range [start, end]. if set is true, return the set bits.
 // else return the unset bits.
 func (b *bitmap) get(start int32, end int32, setBit bool) []*bitmapRs {
+	b.RLock()
+	defer b.RUnlock()
+
+	return b.getWithoutLock(start, end, setBit)
+}
+
+// set sets or cleans the bits in range [start, end]. if setBit is true, set bits. else clean bits.
+func (b *bitmap) set(start int32, end int32, setBit bool) {
+	b.Lock()
+	defer b.Unlock()
+
+	b.setWithoutLock(start, end, setBit)
+}
+
+// lockRange lock the range [start, end], it will set bits in bitmap.
+// if part of range has been set bits, it will return the unset bits range.
+func (b *bitmap) lockRange(start int32, end int32) *lockInfoRs {
+	b.Lock()
+	defer b.Unlock()
+
+	if start < 0 {
+		start = 0
+	}
+
+	if end > b.maxBitIndex {
+		end = b.maxBitIndex
+	}
+
+	rs := &lockInfoRs{}
+	waitChs := []chan struct{}{}
+
+	// get the unset bits range
+	unsetRange := b.getWithoutLock(start, end, false)
+	rs.unsetRange = unsetRange
+
+	// store to lock map to lock range
+	for _, s := range unsetRange {
+		b.lockMap[s.startIndex] = &lockInfo{
+			start:  s.startIndex,
+			end:    s.endIndex,
+			notify: make(chan struct{}),
+		}
+	}
+
+	// get set bits range to fetch the notify chan
+	setRange := b.getWithoutLock(start, end, true)
+	if len(setRange) == 0 {
+		goto end
+	}
+
+	// if the first set bits is start, it means start index of its lock range may be smaller than start.
+	if setRange[0].startIndex == start {
+		if _, ok := b.lockMap[start]; ok {
+			goto fetchWaitCh
+		}
+
+		for i := start - 1; i >= 0; i-- {
+			if info, ok := b.lockMap[i]; ok {
+				waitChs = append(waitChs, info.notify)
+				setRange[0].startIndex = info.end + 1
+				break
+			}
+		}
+	}
+
+fetchWaitCh:
+	for i := 0; i < len(setRange); i++ {
+		lockKey := setRange[i].startIndex
+		for {
+			if lockKey > setRange[i].endIndex {
+				break
+			}
+
+			info, ok := b.lockMap[lockKey]
+			if !ok {
+				// warning: code should not goto the branch
+				break
+			}
+			waitChs = append(waitChs, info.notify)
+			lockKey = info.end + 1
+		}
+	}
+
+	rs.waitChs = waitChs
+
+end:
+	// set bits in the range [start, end]
+	b.setWithoutLock(start, end, true)
+	return rs
+}
+
+// unlockRange remove the range from lockMap and close the notify channel.
+func (b *bitmap) unlockRange(start int32, end int32) {
+	b.Lock()
+	defer b.Unlock()
+
+	b.setWithoutLock(start, end, false)
+
+	info, ok := b.lockMap[start]
+	if !ok {
+		return
+	}
+
+	close(info.notify)
+	delete(b.lockMap, start)
+}
+
+func (b *bitmap) getWithoutLock(start int32, end int32, setBit bool) []*bitmapRs {
 	if end > b.maxBitIndex {
 		end = b.maxBitIndex
 	}
@@ -37,8 +149,6 @@ func (b *bitmap) get(start int32, end int32, setBit bool) []*bitmapRs {
 		start = 0
 	}
 
-	b.RLock()
-	defer  b.RUnlock()
 	rs := []*bitmapRs{}
 	var bit, lastBit uint64
 	var bmp *bitmapRs
@@ -46,7 +156,7 @@ func (b *bitmap) get(start int32, end int32, setBit bool) []*bitmapRs {
 	lastBit = 2
 
 	for {
-		if  start > end {
+		if start > end {
 			break
 		}
 
@@ -59,7 +169,7 @@ func (b *bitmap) get(start int32, end int32, setBit bool) []*bitmapRs {
 			}
 
 			lastBit = bit
-			start ++
+			start++
 			continue
 		}
 
@@ -68,7 +178,7 @@ func (b *bitmap) get(start int32, end int32, setBit bool) []*bitmapRs {
 		}
 
 		lastBit = bit
-		start ++
+		start++
 	}
 
 	if bmp != nil {
@@ -79,7 +189,7 @@ func (b *bitmap) get(start int32, end int32, setBit bool) []*bitmapRs {
 	return rs
 }
 
-func (b *bitmap) set(start int32, end int32, setBit bool) {
+func (b *bitmap) setWithoutLock(start int32, end int32, setBit bool) {
 	if end > b.maxBitIndex {
 		end = b.maxBitIndex
 	}
@@ -88,19 +198,16 @@ func (b *bitmap) set(start int32, end int32, setBit bool) {
 		start = 0
 	}
 
-	b.Lock()
-	defer  b.Unlock()
-
 	second64MinIndex := ((start >> 6) + 1) << 6
 	first64MaxIndex := end
 	if first64MaxIndex >= second64MinIndex {
 		first64MaxIndex = second64MinIndex - 1
 	}
 
-	for i:= start; i <= first64MaxIndex; i ++ {
+	for i := start; i <= first64MaxIndex; i++ {
 		if setBit {
 			b.bm[i>>6] = b.bm[i>>6] | lbm.Bit[i&63]
-		}else{
+		} else {
 			b.bm[i>>6] = b.bm[i>>6] & (^lbm.Bit[i&63])
 		}
 	}
@@ -113,21 +220,34 @@ func (b *bitmap) set(start int32, end int32, setBit bool) {
 	for i := second64MinIndex; i < last64MinIndex; i += 64 {
 		if setBit {
 			b.bm[i>>6] = math.MaxUint64
-		}else{
+		} else {
 			b.bm[i>>6] = 0
 		}
 	}
 
-	for i := last64MinIndex; i <= end; i ++ {
+	for i := last64MinIndex; i <= end; i++ {
 		if setBit {
 			b.bm[i>>6] = b.bm[i>>6] | lbm.Bit[i&63]
-		}else{
+		} else {
 			b.bm[i>>6] = b.bm[i>>6] & (^lbm.Bit[i&63])
 		}
 	}
 }
 
 type bitmapRs struct {
-	startIndex  int32
-	endIndex    int32
+	startIndex int32
+	endIndex   int32
+}
+
+type lockInfo struct {
+	start  int32
+	end    int32
+	notify chan struct{}
+}
+
+type lockInfoRs struct {
+	// rs stores  the range which is locked by caller
+	unsetRange []*bitmapRs
+	//
+	waitChs []chan struct{}
 }
--- dfdaemon/seed/bitmap_test.go
+++ dfdaemon/seed/bitmap_test.go
@@ -2,11 +2,13 @@ package seed
 
 import (
 	"fmt"
-	"github.com/dragonflyoss/Dragonfly/dfget/core/helper"
-	"github.com/go-check/check"
 	"math/rand"
 	"testing"
 	"time"
+
+	"github.com/dragonflyoss/Dragonfly/dfget/core/helper"
+
+	"github.com/go-check/check"
 )
 
 func Test(t *testing.T) {
@@ -32,48 +34,48 @@ func (suite *SeedTestSuite) TestBitMap(c *check.C) {
 	bm.set(0, 100, true)
 	var i int32
 	fmt.Printf("bp1, bm[0]: %x, bm[1]: %x\n", bm.bm[0], bm.bm[1])
-	for i = 0; i <= 100; i ++ {
-		rs := bm.get(i , i, true)
+	for i = 0; i <= 100; i++ {
+		rs := bm.get(i, i, true)
 		c.Assert(len(rs), check.Equals, 1)
 		c.Assert(rs[0].startIndex, check.Equals, i)
 		c.Assert(rs[0].endIndex, check.Equals, i)
 	}
 
-	var start,end int32
+	var start, end int32
 
 	// random 10000 to set [start, end]
-	for i = 0; i <= 1000; i ++ {
+	for i = 0; i <= 1000; i++ {
 		n1 := rand.Int31n(101)
 		n2 := rand.Int31n(101)
 		if n1 < n2 {
 			start = n1
 			end = n2
-		}else {
+		} else {
 			start = n2
 			end = n1
 		}
 
-		rs := bm.get(start , end, true)
+		rs := bm.get(start, end, true)
 		c.Assert(len(rs), check.Equals, 1)
 		c.Assert(rs[0].startIndex, check.Equals, start)
 		c.Assert(rs[0].endIndex, check.Equals, end)
 	}
 
 	bm.set(200, 300, true)
-	rs := bm.get(0,250, true)
+	rs := bm.get(0, 250, true)
 	c.Assert(len(rs), check.Equals, 2)
 	c.Assert(rs[0].startIndex, check.Equals, int32(0))
 	c.Assert(rs[0].endIndex, check.Equals, int32(100))
 	c.Assert(rs[1].startIndex, check.Equals, int32(200))
 	c.Assert(rs[1].endIndex, check.Equals, int32(250))
 
-	rs = bm.get(0,250, false)
+	rs = bm.get(0, 250, false)
 	c.Assert(len(rs), check.Equals, 1)
 	c.Assert(rs[0].startIndex, check.Equals, int32(101))
 	c.Assert(rs[0].endIndex, check.Equals, int32(199))
 
 	bm.set(100, 200, false)
-	rs = bm.get(0,250, false)
+	rs = bm.get(0, 250, false)
 	c.Assert(len(rs), check.Equals, 1)
 	c.Assert(rs[0].startIndex, check.Equals, int32(100))
 	c.Assert(rs[0].endIndex, check.Equals, int32(200))
@@ -90,8 +92,8 @@ func (suite *SeedTestSuite) TestBitMap(c *check.C) {
 	bm.set(315, 318, true)
 	fmt.Printf("index: %d, bp1, bm[4]: %x, bm[5]: %x\n", -4, bm.bm[4], bm.bm[5])
 
-	for i := 0 ;i < 50 ;i ++ {
-		bm.set(int32(300 + i * 5), int32(300 + i * 5 + 3), true)
+	for i := 0; i < 50; i++ {
+		bm.set(int32(300+i*5), int32(300+i*5+3), true)
 	}
 
 	rs = bm.get(0, 1000, true)
@@ -101,8 +103,119 @@ func (suite *SeedTestSuite) TestBitMap(c *check.C) {
 	c.Check(rs[1].startIndex, check.Equals, int32(201))
 	c.Check(rs[1].endIndex, check.Equals, int32(303))
 
-	for i := 0; i < 49 ;i ++ {
-		c.Check(rs[i + 2].startIndex, check.Equals, int32(305 + i * 5))
-		c.Check(rs[i + 2].endIndex, check.Equals, int32(308 + i * 5))
+	for i := 0; i < 49; i++ {
+		c.Check(rs[i+2].startIndex, check.Equals, int32(305+i*5))
+		c.Check(rs[i+2].endIndex, check.Equals, int32(308+i*5))
+	}
+}
+
+func (suite *SeedTestSuite) TestLockBitmap(c *check.C) {
+	bm := newBitMap(100, false)
+	rs1 := bm.lockRange(0, 10)
+	c.Assert(len(rs1.waitChs), check.Equals, 0)
+	c.Assert(len(rs1.unsetRange), check.Equals, 1)
+	c.Assert(rs1.unsetRange[0].startIndex, check.Equals, int32(0))
+	c.Assert(rs1.unsetRange[0].endIndex, check.Equals, int32(10))
+
+	rs2 := bm.lockRange(5, 15)
+	c.Assert(len(rs2.waitChs), check.Equals, 1)
+	c.Assert(len(rs2.unsetRange), check.Equals, 1)
+	c.Assert(rs2.unsetRange[0].startIndex, check.Equals, int32(11))
+	c.Assert(rs2.unsetRange[0].endIndex, check.Equals, int32(15))
+
+	go bm.unlockRange(0, 10)
+	select {
+	case <-time.NewTimer(2 * time.Second).C:
+		c.Fatalf("TestLockBitmap expected not go to timeout")
+	case <-rs2.waitChs[0]:
+		break
+	}
+
+	rs3 := bm.lockRange(15, 20)
+	c.Assert(len(rs3.waitChs), check.Equals, 1)
+	c.Assert(len(rs3.unsetRange), check.Equals, 1)
+	c.Assert(rs3.unsetRange[0].startIndex, check.Equals, int32(16))
+	c.Assert(rs3.unsetRange[0].endIndex, check.Equals, int32(20))
+
+	rs4 := bm.lockRange(1, 19)
+	fmt.Printf("TestLockBitmap, bm[0]: %x, bm lock map: %v\n", bm.bm[0], bm.lockMap)
+	c.Assert(len(rs4.waitChs), check.Equals, 2)
+	c.Assert(len(rs4.unsetRange), check.Equals, 1)
+	c.Assert(rs4.unsetRange[0].startIndex, check.Equals, int32(1))
+	c.Assert(rs4.unsetRange[0].endIndex, check.Equals, int32(10))
+
+	go bm.unlockRange(11, 15)
+	select {
+	case <-time.NewTimer(2 * time.Second).C:
+		c.Fatalf("TestLockBitmap expected not go to timeout")
+	case <-rs3.waitChs[0]:
+		break
+	}
+
+	go bm.unlockRange(16, 20)
+	for i := 0; i < 2; i++ {
+		select {
+		case <-time.NewTimer(2 * time.Second).C:
+			c.Fatalf("TestLockBitmap expected not go to timeout")
+		case <-rs4.waitChs[0]:
+			break
+		case <-rs4.waitChs[1]:
+			break
+		}
+	}
+}
+
+func (suite *SeedTestSuite) TestLockBitmap2(c *check.C) {
+	bm := newBitMap(100, false)
+	rs1 := bm.lockRange(5, 10)
+	c.Assert(len(rs1.waitChs), check.Equals, 0)
+	c.Assert(len(rs1.unsetRange), check.Equals, 1)
+	c.Assert(rs1.unsetRange[0].startIndex, check.Equals, int32(5))
+	c.Assert(rs1.unsetRange[0].endIndex, check.Equals, int32(10))
+
+	rs2 := bm.lockRange(12, 15)
+	c.Assert(len(rs2.waitChs), check.Equals, 0)
+	c.Assert(len(rs2.unsetRange), check.Equals, 1)
+	c.Assert(rs2.unsetRange[0].startIndex, check.Equals, int32(12))
+	c.Assert(rs2.unsetRange[0].endIndex, check.Equals, int32(15))
+
+	rs3 := bm.lockRange(7, 20)
+	c.Assert(len(rs3.waitChs), check.Equals, 2)
+	c.Assert(len(rs3.unsetRange), check.Equals, 2)
+	c.Assert(rs3.unsetRange[0].startIndex, check.Equals, int32(11))
+	c.Assert(rs3.unsetRange[0].endIndex, check.Equals, int32(11))
+	c.Assert(rs3.unsetRange[1].startIndex, check.Equals, int32(16))
+	c.Assert(rs3.unsetRange[1].endIndex, check.Equals, int32(20))
+
+	bm.unlockRange(5, 10)
+	fmt.Printf("TestLockBitmap2, bm[0]: %x, bm lock map: %v\n", bm.bm[0], bm.lockMap)
+	for k, v := range bm.lockMap {
+		fmt.Printf("TestLockBitmap2, bm lock map key: %d, value :%v\n", k, v)
+	}
+
+	timeout := false
+	select {
+	case <-time.NewTimer(2 * time.Second).C:
+		c.Fatalf("TestLockBitmap2 expected not go to timeout")
+		break
+	case <-rs3.waitChs[0]:
+		break
+	}
+
+	select {
+	case <-time.NewTimer(2 * time.Second).C:
+		timeout = true
+		break
+	case <-rs3.waitChs[1]:
+		break
+	}
+
+	c.Check(timeout, check.Equals, true)
+	go bm.unlockRange(12, 15)
+	select {
+	case <-time.NewTimer(2 * time.Second).C:
+		c.Fatalf("TestLockBitmap2 expected not go to timeout")
+	case <-rs3.waitChs[1]:
+		break
 	}
 }
