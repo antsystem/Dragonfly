f9455e64c233f50f5dd4db1ea2709c2694ebfb1e
allen.wq <allen.wq@alipay.com>
2020-05-07 10:31:58 +0800

    add seed interface and implemetation

--- dfdaemon/seed/types.go
+++ dfdaemon/seed/types.go
@@ -17,6 +17,7 @@
 package seed
 
 import (
+	"github.com/dragonflyoss/Dragonfly/pkg/ratelimiter"
 	"time"
 )
 
@@ -53,4 +54,34 @@ type PreFetchResult struct {
 	Err     error
 	// if canceled, caller need not to do other.
 	Canceled bool
+}
+
+type SeedBaseOpt struct {
+	BaseDir string
+	Info    BaseInfo
+
+	downPreFunc func(sd Seed)
+}
+
+type RateOpt struct {
+	DownloadRateLimiter *ratelimiter.RateLimiter
+}
+
+type NewSeedManagerOpt struct {
+	StoreDir           string
+	ConcurrentLimit    int
+	TotalLimit         int
+	DownloadBlockOrder uint32
+	OpenMemoryCache    bool
+
+	// if download rate < 0, means no rate limit; else default limit
+	DownloadRate int64
+	UploadRate   int64
+
+	// water level which is used to expire the seed
+	// if HighLevel is reached, start to prepare the expire
+	HighLevel uint
+
+	// expire will be stopped util water level is smaller than LowLevel.
+	LowLevel uint
 }
